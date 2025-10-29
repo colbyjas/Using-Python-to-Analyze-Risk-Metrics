daaff5eccd80f62be4e38b46e373a666a9f4a61a
colbyjas <colbyjas@buffalo.edu>
2025-10-29 00:11:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 
+# This is a test file
+
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+#%%
+tickers = ['NVDA', 'AAPL', 'META']
+price_data = yf.download(tickers, start = '2020-10-29', end = pd.Timestamp.today().strftime('%Y-%m-%d'))['Close']
+price_data.dropna(inplace=True)
+#%%
+
